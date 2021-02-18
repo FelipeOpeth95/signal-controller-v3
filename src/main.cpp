d83e8f75ec5ba07e1b999cd0fe94743726cb3469
Felipe Gutierrez Morales <fgutie10@eafit.edu.co>
2021-02-18 13:36:50 -0500

    Se incluye y se llama en el main la función transformer 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 
+#include <processors/transformer.h>
+
 void setup() {
   // put your setup code here, to run once:
 }
 
 void loop() {
-  int raw_signal = analogRead(A0);
+  float raw_signal = analogRead(A0);
 
+  float processed_signal = ScaleTransformer :: transform(raw_signal);
 }
