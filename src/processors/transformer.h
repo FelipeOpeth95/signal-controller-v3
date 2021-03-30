93afbce5f4031d54e9aaad272f98a1b9be2b821a
Felipe <fgutie10@eafit.edu.co>
2021-03-30 14:39:19 -0500

    Implementación PID para la trsnformación

--- src/processors/transformer.h
+++ src/processors/transformer.h
@@ -4,9 +4,24 @@
 #include <Arduino.h>
 
 template<class T>
-    T scaler(T input, float scale){
-        float value = map(input,0, 1023,0, 14);
-        return T(value * scale)
+    T PID(T input, float kp, float ki, float kd, float setpoint){
+        
+        float value = map(input,0, 1023,0, 255);
+        
+        float current_time = millis();
+        float elapsed_time = current_time - previous_time;
+
+        float error = setpoint - value;
+        float acum_error = error + (error * elapsed_time;
+
+        float rate_error = (error - last_error)/elapsed_time;
+
+        float output = kp*error + ki*acum_error + kd*rate_error;
+
+        float last_error = error;
+        float previous_time = current_time;
+        
+        return T(output)
     }
 
 #endif
